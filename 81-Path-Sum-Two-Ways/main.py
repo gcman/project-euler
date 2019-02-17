8dcd72d5a4134f9e1f114d6fa5f8add80ef3ae67
Gautam Manohar <gcmanohar001@gmail.com>
2019-02-17 16:50:52 -0500

    Euler 81, 87

--- 81-Path-Sum-Two-Ways/main.py
+++ 81-Path-Sum-Two-Ways/main.py
@@ -1,12 +1,16 @@
 N = int(input())
-G = [list(map(int,input().split())) for _ in range(N)]
+G = [list(map(int, input().split())) for _ in range(N)]
 
-for i in range(N-2,-1,-1):
-	G[N-1][i] += G[N-1][i+1]
-	G[i][N-1] += G[i+1][N-1]
+# MPS = minimal path sum
+# Find MPS for bottom row and rightmost col
+for i in range(N-2, -1, -1):
+    G[N-1][i] += G[N-1][i+1]
+    G[i][N-1] += G[i+1][N-1]
 
-for i in range(N-2,-1,-1):
-	for j in range(N-2,-1,-1):
-		G[i][j] += min(G[i+1][j],G[i][j+1])
+# Find MPS for each square starting at bot-right
+for i in range(N-2, -1, -1):
+    for j in range(N-2, -1, -1):
+        G[i][j] += min(G[i+1][j], G[i][j+1])
 
+# Return topleft value
 print(G[0][0])
--- 87-Prime-Power-Triples/main.py
+++ 87-Prime-Power-Triples/main.py
@@ -1,40 +1,45 @@
-# Sieve of Eratosthenes
-def primes(n):
-	primes = []
-	sieve = [True] * (n + 1)
-	for p in range(2, n + 1):
-		if sieve[p]:
-			primes.append(p)
-			for i in range(p * p, n + 1, p):
-				sieve[i] = False
-	return primes
-
-# Binary search
 def bs(arr, l, r, x):
-	while l <= r:
-		mid = l + (r - l)//2;
-		if arr[mid] == x:
-			return mid + 1
-		elif arr[mid] < x:
-			l = mid + 1
-		else:
-			r = mid - 1
-	return r + 1
+    """Binary Search arr from index l to r for x"""
+    while l <= r:
+        mid = l + (r - l)//2
+        if arr[mid] == x:
+            return mid + 1
+        elif arr[mid] < x:
+            l = mid + 1
+        else:
+            r = mid - 1
+    return r + 1
 
-MAX = int(5e7)
+
+def primes(n):
+    """Sieve of Eratosthenes"""
+    primes = []
+    sieve = [True] * (n + 1)
+    for p in range(2, n + 1):
+        if sieve[p]:
+            primes.append(p)
+            # Cross out all multiples of p
+            for i in range(p * p, n + 1, p):
+                sieve[i] = False
+    return primes
+
+
+# Create lists of valid prime powers
+MAX = int(1e7)
 P = primes(int(MAX**(1./2.)))
 p2 = [p**2 for p in P]
 p3 = [p**3 for p in P if p < MAX**(1./3.)]
 p4 = [p**4 for p in P if p < MAX**(1./4.)]
 
+# Keep unique sums
 sums = set()
 for a in p2:
-	for b in p3:
-		for c in p4:
-			sums.add(a+b+c)
+    for b in p3:
+        for c in p4:
+            sums.add(a+b+c)
 sums = list(sorted(sums))
 
 T = int(input())
 for _ in range(T):
-	N = int(input())
-	print(bs(sums,0,len(sums)-1,N))
+    N = int(input())
+    print(bs(sums, 0, len(sums)-1, N))
